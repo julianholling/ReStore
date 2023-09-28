071d8d7f6becef5f10480c80031b48fde4afb089
Julian Holling <julian.holling@ms-3.co.uk>
2023-09-28 08:58:15 +0100

    chapter 41 - Adding NavLinks to AppBar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,17 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const appBarLinksLeft = [
+    {title: 'catalog', path: '/catalog' },
+    {title: 'about', path: '/about' },
+    {title: 'contact', path: '/contact' },
+]
+
+const appBarLinksRight = [
+    {title: 'login', path: '/login' },
+    {title: 'register', path: '/register' },
+]
 
 interface Props{
     darkMode: boolean;
@@ -10,8 +23,38 @@ export default function Header({darkMode, themeChangeEventHandler}: Props) {
     return(
         <AppBar position='static' sx={{mb: 4}} >
             <Toolbar>
-                <Typography variant="h6">REACT STORE</Typography>
+                <Typography 
+                    variant="h6" 
+                    component={NavLink} 
+                    to='/'
+                    sx={{cololr: 'inherit', textDecoration: 'none'}}    
+                >
+                    
+                    REACT STORE
+                </Typography>
+                
                 <Switch checked={darkMode} onChange={themeChangeEventHandler}></Switch>
+
+                <List sx={{display: 'flex'}}>
+                  {appBarLinksLeft.map(({title, path}) => (
+                  <ListItem component={NavLink} to={path} key={path} sx={{color: 'inherit', typography: 'h6'}}>
+                    {title.toUpperCase()}
+                  </ListItem>))}  
+                </List>
+
+                <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+                    <Badge badgeContent='4' color='secondary'>
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
+
+                <List sx={{display: 'flex'}}>
+                  {appBarLinksRight.map(({title, path}) => (
+                  <ListItem component={NavLink} to={path} key={path} sx={{color: 'inherit', typography: 'h5'}}>
+                    {title.toUpperCase()}
+                  </ListItem>))}  
+                </List>
+            
             </Toolbar>
         </AppBar>
     )
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from '../layout/App';
 import HomePage from '../../features/home/HomePage';
 import ProductDetails from '../../features/catalog/ProductDetails';
