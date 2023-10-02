030462f61d0dd31a12b5c6b4d8ad61224266ef91
Julian Holling <julian.holling@ms-3.co.uk>
2023-10-02 11:32:09 +0100

    chapter 54 - creating a server error component

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { router } from "../router/Routes";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
@@ -28,7 +29,7 @@ axios.interceptors.response.use(response => {
                 toast.error(data.title);
                 break;
             case 500:
-                toast.error(data.title);
+                router.navigate('/server-error', {state: {error:data}});       //  This is OK even though react says its a private method
                 break;
             default:
                 break;
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -5,6 +5,7 @@ import ProductDetails from '../../features/catalog/ProductDetails';
 import Catalog from '../../features/catalog/Catalog';
 import AboutPage from '../../features/about/AboutPage';
 import ContactPage from '../../features/contact/ContactPage';
+import ServerError from '../errors/ServerError';
 
 export const router = createBrowserRouter(
     [
@@ -17,6 +18,7 @@ export const router = createBrowserRouter(
                 {path: 'catalog/:id', element: <ProductDetails />},
                 {path: 'about', element: <AboutPage />},
                 {path: 'contact', element: <ContactPage />},
+                {path: 'server-error', element: <ServerError />},
             ]
         }
     ]
