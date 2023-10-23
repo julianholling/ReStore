c2fe1cdf7950a045fff1f4aaa085bac614c9c9cd
Julian Holling <julian.holling@ms-3.co.uk>
2023-10-23 14:25:33 +0100

    chapter 87 -action creators

--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -11,14 +11,39 @@ const initialState: CounterState = {
     title: 'YARC (yet another redux counter)'
 }
 
-export default function counterReducer(state = initialState, action: any){
+export function increment(amount = 1){
+    return {
+        type: INCREMENT_COUNTER,
+        payload: amount
+    }
+}
+
+export function decrement(amount = 1){
+    return {
+        type: DECREMENT_COUNTER,
+        payload: amount
+    }
+}
+
+interface CounterAction {
+    type: string,
+    payload: number
+}
+
+export default function counterReducer(state = initialState, action: CounterAction){
     switch(action.type) 
     {
         case "INCREMENT_COUNTER":
-            return { ...state, data: state.data + 1}
+            return { 
+                ...state, 
+                data: state.data + action.payload
+            }
             break;
         case "DECREMENT_COUNTER":
-            return { ...state, data: state.data - 1}
+            return { 
+                ...state, 
+                data: state.data - action.payload
+            }
             break;
         default:
             return state;
