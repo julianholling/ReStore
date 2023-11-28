611d56258bb82484087691f397af98aad2a99ece
Julian Holling <julian.holling@ms-3.co.uk>
2023-11-28 09:56:04 +0000

    chapter 92 - Redux Async Functions Part 1

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -1,13 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Basket } from "../../app/models/basket";
+import agent from "../../app/api/agent";
 
 interface BasketState {
-    basket: Basket | null
+    basket: Basket | null,
+    status: string
 }
 
 const initialState: BasketState = {
-    basket: null
+    basket: null,
+    status: 'idle'
 }
+
+export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity: number}>(
+    'basket/addBasketItemAsync',
+    async({productId, quantity}) => {
+        try {
+            return await agent.Basket.addItem(productId, quantity)
+        } 
+        catch(error){
+            console.log(error)
+        }
+    }
+)
 
 export const basketSlice = createSlice({
     name: 'basket',
@@ -25,7 +40,24 @@ export const basketSlice = createSlice({
                 state.basket.items.splice(itemIndex, 1);
             }
         }
-    }
+    },
+    extraReducers: (builder => {
+        builder.addCase(addBasketItemAsync.pending, (state, action) => {
+            console.log(action);
+            state.status = 'pendingAddItem';
+        });
+
+        builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+            console.log(action);
+            state.basket = action.payload;
+            state.status = 'idle'
+        });
+
+        builder.addCase(addBasketItemAsync.rejected, (state) => {
+            state.status = 'idle'
+
+        });
+    })
 })
 
 export const {setBasket, removeItem} = basketSlice.actions;
