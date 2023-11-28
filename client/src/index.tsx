4f296bd32c5c6a9ab4d146f355e0f0c07752cd14
Julian Holling <julian.holling@ms-3.co.uk>
2023-11-28 09:26:13 +0000

    chapter 91 - refactor for redux store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
-import { StoreProvider } from './app/context/StoreContext';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore';
 
@@ -17,11 +16,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <StoreProvider>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
-
-    </StoreProvider>
   </React.StrictMode>
 );
