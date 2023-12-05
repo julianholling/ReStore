d4991e8a4c80cec740760870d7eb87df34539919
Julian Holling <julian.holling@ms-3.co.uk>
2023-12-05 09:32:24 +0000

    chapter 131 - config swagger to send JWT

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(configuration => 
+{
+    var jwtSecuritySecheme = new OpenApiSecurityScheme{
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put bearer plus your token in the box below ...",
+        Reference = new OpenApiReference 
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme,
+        }
+    };
+
+    configuration.AddSecurityDefinition(jwtSecuritySecheme.Reference.Id, jwtSecuritySecheme);
+    configuration.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        {
+            jwtSecuritySecheme, Array.Empty<string>()
+        }
+    });
+    
+});
 
 builder.Services.AddDbContext<StoreContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -53,7 +78,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(configuration => 
+    {
+        configuration.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
 }
 
 app.UseCors(options => {
