b5074b49d566de7a40a64bc2dc986deefb25e3f7
Julian Holling <julian.holling@ms-3.co.uk>
2023-12-27 10:51:07 +0000

    chapter 134 - Controlled Cmpnnt - Login fixed typo

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -18,7 +18,7 @@ export default function Login() {
     })
 
   const submitLoginEventHandler = (event: any) => {
-    event.preventdefault();
+    event.preventDefault();
     agent.Account.login(values);
   };
 
