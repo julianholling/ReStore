d5aedafb9c475e4cc5ab687a1baefa0238052f18
Julian Holling <julian.holling@ms-3.co.uk>
2024-01-17 13:15:32 +0000

    chapter 210 - a reusable select list (dropdown)

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
+import useProducts from "../../app/hooks/useProducts";
+import AppSelectList from "../../app/components/AppSelectList";
 
 interface Props {
     product? : Product;
@@ -12,6 +14,7 @@ interface Props {
 export default function ProductForm({product, cancelEdit }: Props) {
 
     const { control, reset } = useForm();
+    const { brands, types } = useProducts();
 
     useEffect(() => {
         if(product)
@@ -30,10 +33,10 @@ export default function ProductForm({product, cancelEdit }: Props) {
                     <AppTextInput control={control} name='name' label='Product name' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='brand' label='Brand' />
+                    <AppSelectList control={control} items={brands} name='brand' label='Brand' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='type' label='Type' />
+                    <AppSelectList control={control} items={types} name='type' label='Type' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <AppTextInput control={control} name='price' label='Price' />
