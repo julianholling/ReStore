cc4fa0160fadf945a7a83d02bd4d06f0b3e27957
Julian Holling <julian.holling@ms-3.co.uk>
2024-01-17 10:48:30 +0000

    chapter 207 - updating and deleting images

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -99,7 +99,7 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm] UpdateProductDto productDto)
         {
 
             var product = await _context.Products.FindAsync(productDto.Id);
@@ -111,11 +111,29 @@ namespace API.Controllers
 
             _mapper.Map(productDto, product);
 
+            if(productDto.File is not null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+                if(imageResult.Error is not null)
+                {
+                    return BadRequest(new ProblemDetails {Title = imageResult.Error.Message});
+                }
+
+                if(!string.IsNullOrWhiteSpace(product.PublicId))
+                {
+                    await _imageService.DeleteImageAsync(product.PublicId);
+                }
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
 
             if(result)
             {
-                return NoContent();
+                return Ok(product);
             }
 
             return BadRequest(new ProblemDetails{Title = "Error updating product"});
@@ -131,6 +149,11 @@ namespace API.Controllers
             if(product is null)
             {
                 return NotFound();
+            }
+
+            if (!string.IsNullOrWhiteSpace(product.PublicId))
+            {
+                await _imageService.DeleteImageAsync(product.PublicId);
             }
 
             _context.Products.Remove(product);
